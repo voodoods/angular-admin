83e599e287237f07f3131a1f2f60c8cadfae344c
Norman Steger <me@normansteger.com>
2020-03-01 14:17:00 +0100

    split up layout into separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppDashboardComponent } from './app-dashboard/app-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppHeaderComponent,
+    AppDashboardComponent,
   ],
   imports: [
     BrowserModule,
