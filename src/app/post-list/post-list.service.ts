0254444e2c4e90b1209d4c4e508323411476dfb5
Norman Steger <n.steger@reply.de>
2020-03-13 16:05:50 +0100

    use post list service host from environment variables

--- src/app/post-list/post-list.service.ts
+++ src/app/post-list/post-list.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IBlogPost } from './types'
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostListService {
-  private postsResourceUrl = 'http://localhost:4001/posts'
+  private postListServiceURL = `${environment.POST_LIST_SERVICE_HOST}/posts`
 
   constructor(
     private http: HttpClient
   ) { }
 
-  getPostsList() : Observable<IBlogPost[]> {
-    return this.http.get<IBlogPost[]>(this.postsResourceUrl)
+  getPostList() : Observable<IBlogPost[]> {
+    return this.http.get<IBlogPost[]>(this.postListServiceURL)
   }
 }
