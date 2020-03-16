3a47511f7f9df1effcad766b8444d76faa4a9bce
Norman Steger <n.steger@reply.de>
2020-03-16 15:55:38 +0100

    add users mock api endpoint

--- .config/json-server/db.js
+++ .config/json-server/db.js
@@ -1,4 +1,5 @@
 module.exports = () => ({
   "posts": require('./data/blog-posts-list.json'),
-  "details-post-1": require('./data/blog-post-1-details.json')
+  "details-post-1": require('./data/blog-post-1-details.json'),
+  "users": require('./data/users.json')
 })
